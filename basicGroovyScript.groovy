c641452d336c9433ff8ad282310f35794614d505
arkytechie <40334092+arkytechie@users.noreply.github.com>
2021-04-03 10:02:04 +0200

    basic groovy variables and prints

--- basicGroovyScript.groovy
+++ basicGroovyScript.groovy
@@ -1,1 +1,8 @@
-print ('Hello World...!!')
+print ('Hello World...!! \n')
+println 'Hello World 2 !!'
+
+def name = "Elon Musk"
+print ('My name is ${name}')
+print ("My name is ${name}")
+
+println ("My name is ${name}")
